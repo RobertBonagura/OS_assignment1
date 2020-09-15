4471f46a9ac25d0c66f3c01528ff8b07049b2b37
RobertBonagura <rjb348@butter.cs.rutgers.edu>
2020-09-15 14:26:23 -0400

    First implementation of first two parts

--- TimeFork.c
+++ TimeFork.c
@@ -1,25 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 // Place any necessary global variables here
+int ITERATIONS = 5000;
 
 int main(int argc, char *argv[]){
 
-	/*
-	 *
-	 * Implement Here
-	 *
-	 *
-	 */
-        
-        // Start timer
+        // 1. Save start time using gettimeofday().
+        struct timeval start, end;
+        gettimeofday(&start, NULL);
 
         // For 5,000 times:
         //      1. Call fork() and then wait() or waitpid()
         //      2. Child process should immediately terminiate with exit() or return
-        
-        // Calulcae time elapsed and average fork()
+        for (int i = 0; i < ITERATIONS; i++){
+                if (fork() == 0){
+                        exit(0);
+                } else {
+                        wait(NULL);
+                }
+        }
 
+        // 3. Calculate time elapsed and average syscall using floating point arithemtic. 
+        gettimeofday(&end, NULL);
+
+        long seconds = (end.tv_sec - start.tv_sec);
+        long microseconds = ((seconds * 1000000) + end.tv_usec ) - (start.tv_usec);
+        printf("Time elapsed is %ld seconds and %ld miliseconds\n", seconds, microseconds);
 
 	return 0;
 
--- TimeSysCall.c
+++ TimeSysCall.c
@@ -1,23 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 // Place any necessary global variables here
+int ITERATIONS = 100000;
 
 int main(int argc, char *argv[]){
 
-	/*
-	 *
-	 * Implement Here
-	 *
-	 *
-	 */
-        
         // 1. Save start time using gettimeofday().
+        struct timeval start, end;
+        gettimeofday(&start, NULL);
 
         // 2. Run getpid() 100,000 times.
+        for (int i = 0; i < ITERATIONS; i++){
+                getpid();
+        }
 
         // 3. Calculate time elapsed and average syscall using floating point arithemtic. 
+        gettimeofday(&end, NULL);
 
+        long seconds = (end.tv_sec - start.tv_sec);
+        long microseconds = ((seconds * 1000000) + end.tv_usec ) - (start.tv_usec);
+        printf("Time elapsed is %ld seconds and %ld miliseconds\n", seconds, microseconds);
 	return 0;
 
 }
